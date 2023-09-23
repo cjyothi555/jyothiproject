01fecc0b76be03a583767bcf85c1ae906ed936d4
cjyothi555 <jyothicherukuri555@gmail.com>
2023-09-23 11:20:40 +0530

    first version released from bash

---
+++ src/main/java/com/example/jyothiproject/JyothiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.jyothiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JyothiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JyothiprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jyothiproject/JyothiprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jyothiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JyothiprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
